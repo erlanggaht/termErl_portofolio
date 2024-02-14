37b7c292966d262ee1620620876ef703696f353a
Erlangga Hidayatullah <erlanggahidayat.md@gmail.com>
2024-02-14 11:24:16 +0700

    refactor: type Text

--- src/app/utility/Text/Home/text.tsx
+++ src/app/utility/Text/Home/text.tsx
@@ -1,16 +1,5 @@
-import React, { ReactElement } from "react"
 import { htmlAbout, htmllist_command_Route,htmllist_command_CatFile,htmllist_command_Other,html_ls_pwd } from "./htmlText"
-
-type TypeTeks = {
-    about?: ReactElement,
-    list_command?:{
-        htmllist_command_Route?: React.ReactNode[],
-        htmllist_command_CatFile?: React.ReactNode[]
-        htmllist_command_Other?: React.ReactNode[]
-    },  
-    list_directory:  React.ReactNode
-    
-}
+import TypeTeks from "@/app/@types/typeText"
 
 function Teks(): TypeTeks  {
     return {
--- src/app/utility/Text/Projects/text.tsx
+++ src/app/utility/Text/Projects/text.tsx
@@ -1,17 +1,5 @@
-import React, { ReactElement } from "react"
+import TypeTeks from "@/app/@types/typeText"
 import {htmllist_command_CatFile,htmllist_command_RunFile,htmllist_command_Other,html_ls_pwd } from "./htmlText"
-
-type TypeTeks = {
-    about?: ReactElement,
-    list_command?:{
-        htmllist_command_Route?: React.ReactNode[],
-        htmllist_command_CatFile?: React.ReactNode[],
-        htmllist_command_RunFile?: React.ReactNode,
-        htmllist_command_Other?: React.ReactNode[]
-    },  
-    list_directory:  React.ReactNode
-    
-}
 
 function Teks(): TypeTeks  {
     return {
